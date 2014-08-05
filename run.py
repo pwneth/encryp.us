8cc31f9bb013727a6ada52ee078d85e1b378d700
Michael <mpn123@gmail.com>
2014-08-05 12:36:43 +0200

    added aes server side encryption

--- run.py
+++ run.py
@@ -5,6 +5,11 @@ import tornado.httputil
 import base64
 import json
 from datetime import datetime, date
+from Crypto.Cipher import AES
+
+#temporary hardcodes AES key for encrypting messages -- will
+#use user generated key later on
+obj=AES.new("1qazxsw23edcvfr4")
 
 #this is temporary -- will take users from a JSON db later on
 #will also allow for user/pw creation by admin
@@ -18,6 +23,27 @@ def loadjson():
 		with open('static/data.json') as f:
 			jsondata = json.load(f)
 		return jsondata
+
+
+#class decrypts the message object contents
+class decrypt_msg(object):
+	def __init__(self, msg):
+		self.name = msg['name']
+		self.time = msg['time']
+		message = base64.b64decode(msg['message'])
+		self.msg = obj.decrypt(message).decode("utf-8")
+
+
+#loads json data and append decrypted information to an array and return
+#it for the templates to use
+def append_messages():
+	json_data = loadjson()
+	messages = []
+
+	for data in json_data['messages']:
+		messages.append(decrypt_msg(data))
+
+	return messages
 
 
 class BaseHandler(tornado.web.RequestHandler):
@@ -37,7 +63,7 @@ class MessageHandler(BaseHandler):
 
 	def render_now(self, future):
 		self.render("home.html", title="Home Page", 
-			username=self.current_user, messages=loadjson())
+			username=self.current_user, messages=append_messages())
 		# self.finish()
 
 
@@ -46,16 +72,27 @@ class MainHandler(BaseHandler):
 	@tornado.web.authenticated
 	def get(self):
 		self.render("home.html", title="Home Page", 
-			username=self.current_user, messages=loadjson())
+			username=self.current_user, messages=append_messages())
 
 	def post(self):
 		msg = self.get_argument("message")
+
+		#make sure msg is 16 bits
+		if ((len(msg) % 16) != 0):
+			times = 16 - (len(msg) % 16)
+			for i in range(1, times+1):
+				msg += " "
+
+		#encrypt the message with b64 and utf-8 encoding
+		encrypted_msg = base64.b64encode(obj.encrypt(msg)).decode("utf-8")
 		time = datetime.now().strftime("%Y-%m-%d %H:%M")
 		
 		with open('static/data.json') as f:
 			data = json.load(f)
 
-		data['messages'].append({'name':self.current_user.decode("utf-8"),'message':msg,'time':time})
+		data['messages'].append({'name':self.current_user.decode("utf-8"),
+								 'message':encrypted_msg,
+								 'time':time})
 
 		with open('static/data.json', 'w') as f:
 			json.dump(data, f)
