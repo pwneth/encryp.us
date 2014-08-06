02b6e6d376feb3b9d5713a816230c294245c1f89
Michael <mpn123@gmail.com>
2014-08-06 16:09:39 +0200

    added encryption key to login page in local session variable

--- static/main.js
+++ static/main.js
@@ -1,27 +1,42 @@
 $(document).ready(function() {
 
-	var password = "";
+	var session_password = sessionStorage.getItem("session_password");
 
 	function decrypt_messages() {
 		$(".msg").each(function( index ) {
 			var encrypted_content = $(this).html().trim();
-			var decrypted_content = CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(encrypted_content, password));
+			var decrypted_content = CryptoJS.enc.Utf8.stringify(CryptoJS.AES.decrypt(encrypted_content, session_password));
 			$(this).text(decrypted_content);
 		});
 	}
+	decrypt_messages();
 
-	$("#aes_submit").click(function() {
-		password = $("#aes_input").val();
-		if (password == "") {
-			$("#aes_submit").effect( "highlight", {color: 'red'}, 1000 );
-			$("#aes_submit").attr("value", "can't be empty");
-		} else {
-			$('#aes_password').hide();
-			$('#chat').show();
-			$("#chat_input").focus();
-			decrypt_messages();
-		}
-		return false;
+
+	// if (sessionStorage.getItem("session_password")) {
+	// 	session_password = sessionStorage.getItem("session_password");
+	// 	$('#aes_password').hide();
+	// 	$('#chat').show();
+	// 	$("#chat_input").focus();
+	// 	decrypt_messages();
+	// } else {
+	// 	var session_password = "";
+	// }
+
+	$("#login_submit").click(function() {
+		var password = $("#enc_key").val();
+		sessionStorage.setItem("session_password", password);
+		$("#enc_key").val("xxxxxx");
+		// if (password == "") {
+		// 	$("#aes_submit").effect( "highlight", {color: 'red'}, 1000 );
+		// 	$("#aes_submit").attr("value", "can't be empty");
+		// } else {
+		// 	session_password = sessionStorage.getItem("session_password");
+		// 	$('#aes_password').hide();
+		// 	$('#chat').show();
+		// 	$("#chat_input").focus();
+		// 	decrypt_messages();
+		// }
+		// return false;
 	});
 
 	function load_messages(){
@@ -66,7 +81,7 @@ $(document).ready(function() {
 			$("#chat_submit").attr("value", "can't be empty");
 			return false;
 		} else {
-			var encrypted_message = CryptoJS.AES.encrypt(message, password);
+			var encrypted_message = CryptoJS.AES.encrypt(message, session_password);
 
 			$.ajax({
 	            type: "POST",
